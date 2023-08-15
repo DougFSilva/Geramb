7f8fe2a75d44e4581317a2ccaab7573fad5c8330
Douglas Ferreira da Silva <doiug.ferreira.dfs@gmail.com>
2023-08-15 17:31:02 -0300

    Concluidas classes UpdateStudantClass e DeleteStudantClass

--- src/main/java/com/dougFerreira/Geramb/services/studantClass/DeleteStudantClass.java
+++ src/main/java/com/dougFerreira/Geramb/services/studantClass/DeleteStudantClass.java
@@ -1,5 +1,22 @@
 package com.dougFerreira.Geramb.services.studantClass;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dougFerreira.Geramb.models.StudantClass;
+import com.dougFerreira.Geramb.repositories.StudantClassRepository;
+
+@Service
 public class DeleteStudantClass {
 
+	@Autowired
+	private FindStudantClass findStudantClass;
+	
+	@Autowired
+	private StudantClassRepository repository;
+	
+	public void delete(Long id) {
+		StudantClass studantClass = findStudantClass.findById(id);
+		repository.delete(studantClass);
+	}
 }
--- src/main/java/com/dougFerreira/Geramb/services/studantClass/UpdateStudantClass.java
+++ src/main/java/com/dougFerreira/Geramb/services/studantClass/UpdateStudantClass.java
@@ -1,5 +1,26 @@
 package com.dougFerreira.Geramb.services.studantClass;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dougFerreira.Geramb.models.StudantClass;
+import com.dougFerreira.Geramb.repositories.StudantClassRepository;
+
+@Service
 public class UpdateStudantClass {
 
+	@Autowired
+	private FindStudantClass findStudantClass;
+	
+	@Autowired
+	private StudantClassRepository repository;
+	
+	public StudantClass update(Long id, Integer number, String name, String description, String technologialArea) {
+		StudantClass studantClass = findStudantClass.findById(id);
+		studantClass.setNumber(number);
+		studantClass.setName(name);
+		studantClass.setDescription(description);
+		studantClass.setTechnologicalArea(technologialArea);
+		return repository.save(studantClass);
+	}
 }
